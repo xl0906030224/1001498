54db35644b5d6fcc6b32cdc4af9ef8f3cf35a8d5
许良 <xuliangbinqi@outlook.com>
2017-04-14 16:53:10 +0800

    test4

--- src/com/example/testant/MainAntActivity.java
+++ src/com/example/testant/MainAntActivity.java
@@ -11,7 +11,7 @@ public class MainAntActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_ant);
-		//1
+		//2
 		
 	}
 }

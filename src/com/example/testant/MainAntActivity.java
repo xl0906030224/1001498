d04cfa2217cd7acdaefb0b9e89282b5281eb00f8
许良 <xuliangbinqi@outlook.com>
2017-04-14 16:21:56 +0800

    test2

--- src/com/example/testant/MainAntActivity.java
+++ src/com/example/testant/MainAntActivity.java
@@ -6,10 +6,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainAntActivity extends Activity {
-  
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_ant);
+		
 	}
 }

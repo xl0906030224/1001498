2aabb20079c17735abb1587af6306240fe09c341
许良 <xuliangbinqi@outlook.com>
2017-04-14 09:43:34 +0800

    上传代码

---
+++ src/com/example/testant/MainAntActivity.java
@@ -0,0 +1,15 @@
+package com.example.testant;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainAntActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_ant);
+	}
+}

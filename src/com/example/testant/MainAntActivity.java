f2f6c9d19ec934350e578728a955da33a5d6f1ac
许良 <xuliangbinqi@outlook.com>
2017-04-14 16:14:48 +0800

    test1

--- src/com/example/testant/MainAntActivity.java
+++ src/com/example/testant/MainAntActivity.java
@@ -6,7 +6,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainAntActivity extends Activity {
-
+  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
